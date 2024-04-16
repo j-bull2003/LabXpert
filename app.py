07ff04eaea90e2c112a549751b8580c79552fdc4
j-bull2003 <jessicabull833@gmail.com>
2024-04-16 17:06:09 +0100

    db

--- app.py
+++ app.py
@@ -62,8 +62,9 @@ def run_research_assistant_chatbot():
     st.caption('Analyse your experimental data')
     st.markdown('Your personal Data Anaylist tool ')
     st.divider()
+    CHROMA_PATH = "https://drive.google.com/uc?export=download&id=1aXR-lXPU2vlBkm-Ry-rIyhHxK-I6MUCe"
 
-    CHROMA_PATH = "chroma"
+    # CHROMA_PATH = "chroma"
     # CHROMA_PATH = "https://chromadump.s3.eu-west-2.amazonaws.com/chroma/"
     PROMPT_TEMPLATE = """
     Answer the question based only on the following context:
