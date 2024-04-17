c1e862568c598d2e6b11fef5f9fcf0e69cf7ab79
j-bull2003 <jessicabull833@gmail.com>
2024-04-17 20:26:47 +0100

    sq

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ from langchain.chat_models import ChatOpenAI
 from datetime import datetime
 
 
-# __import__('pysqlite3')
-# import sys
-# sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
   # Lower complexity
 def estimate_complexity(question):
     # List of complex keywords
