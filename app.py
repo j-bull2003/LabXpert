ee1d1d124a4f11fa323f1276cf64b01980dd6082
j-bull2003 <jessicabull833@gmail.com>
2024-04-19 11:32:53 +0100

    st.image("pic.png")

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ def run_research_assistant_chatbot():
         st.session_state.messages = load_chat_history()
 
     with st.sidebar:
-        st.image("pic.png")
+        # st.image("pic.png")
         if st.button("Delete Chat History"):
             st.session_state.messages = []
             save_chat_history([])
@@ -410,7 +410,7 @@ def run_data_analysis_chatbot():
         
         assistant = client.beta.assistants.retrieve(assistant_id)
         with st.sidebar:
-            st.image("pic.png")
+            # st.image("pic.png")
             # assistant_name = st.text_input("Name", value = assistant.name)
             assistant_instructions = "You are a data analyst"
             model_option = 'gpt-3.5-turbo-0125'
@@ -604,6 +604,7 @@ def main():
     st.sidebar.title("LabXpert 🧬")
     # Set 'Research Xpert 🔬' as the default selected option
     chatbot_mode = st.sidebar.radio("Select an AI Xpert", ('Research Xpert 📄', 'Data Analysis 📊'), index=0)
+    st.image("pic.png")
     if chatbot_mode == 'Research Xpert 📄':
         init_research_assistant()
         run_research_assistant_chatbot()
