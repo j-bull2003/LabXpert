26f7c9c121d0b1c40f071ce29ea19fd24728e25a
j-bull2003 <jessicabull833@gmail.com>
2024-04-19 17:12:49 +0100

    thres

--- app.py
+++ app.py
@@ -215,14 +215,14 @@ def run_research_assistant_chatbot():
         k = estimate_complexity(prompt)
         results = db.similarity_search_with_relevance_scores(prompt_with_history, k=k)
         with st.spinner("Thinking..."):
-            if len(results) == 0 or results[0][1] < 0.9:
+            if len(results) == 0 or results[0][1] < 0.85:
                 model = ChatOpenAI(openai_api_key=openai_api_key, model_name="gpt-3.5-turbo-0125")
                 # query the assistant here instead
                 response_text = model.predict(prompt_with_history)      
                 response = f" {response_text}"
                 a = estimate_complexity(prompt)
                 follow_up_results = db.similarity_search_with_relevance_scores(response_text, k=a)
-                very_strong_correlation_threshold = 0.8
+                very_strong_correlation_threshold = 0.75
                 high_scoring_results = [result for result in follow_up_results if result[1] >= very_strong_correlation_threshold]
                 if high_scoring_results:
                     sources = []
