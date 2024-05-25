20588d8f26799ac8078a0640074f55eccbe386ca
j-bull2003 <jessicabull833@gmail.com>
2024-05-25 18:09:50 +0100

    hide

--- pages/02-LabXpert.py
+++ pages/02-LabXpert.py
@@ -806,9 +806,15 @@ if __name__ == '__main__':
             </style>
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    hide_github_icon = """
-        #GithubIcon {
-        visibility: hidden;
-        }
-        """
-    st.markdown(hide_github_icon, unsafe_allow_html=True)
+    st.markdown(
+    """
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob,
+    .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137,
+    .viewerBadge_text__1JaDK {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
