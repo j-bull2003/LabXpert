1b9d98aa18050f70a9cd5679a9c088c60af22ec8
j-bull2003 <jessicabull833@gmail.com>
2024-05-25 18:12:31 +0100

    hide icons

--- 01-Sign_Up.py
+++ 01-Sign_Up.py
@@ -93,6 +93,14 @@ hide_streamlit_style = """
             </style>
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.markdown(
     """
--- pages/02-LabXpert.py
+++ pages/02-LabXpert.py
@@ -806,6 +806,14 @@ if __name__ == '__main__':
             </style>
             """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+    hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    </style>
+    """
+
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     st.markdown(
     """
     <style>
